96aea3780aab962a5bdaa3358f3b90b71a6623a0
Antony Thomas <134713648+torcodex@users.noreply.github.com>
2025-04-08 15:16:15 +0530

    Update TCP_SERVER.c
    TCP is connection oriented
    Congestion Control:
    Reduces data transfer rate when the network is congested, to avoid packet loss.
    Reliable:
    It ensures that the packet is delivered

--- TCP_SERVER.c
+++ TCP_SERVER.c
@@ -1,54 +1,56 @@
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-int main(void) {
-int socket_desc,client_sock,client_size;
-struct sockaddr_in server_addr,client_addr;
-char server_message[2000],client_message[2000];
-memset(server_message,'\0',sizeof(server_message));
-memset(client_message,'\0',sizeof(client_message));
-socket_desc = socket(AF_INET,SOCK_STREAM,0);
-if (socket_desc < 0) {
-printf("Err while creating socket\n");
-return -1;
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h
+int main(void){
+    int sockid, client_sock, client_size;
+    struct sockaddr_in server,client;
+    char s_message[2000],c_message[2000];
+
+    memset(s_message,'\0',sizeof(s_message));
+    memset(c_message,'\0',sizeof(c_message));
+
+    sockid=socket(AF_INET,SOCK_STREAM,0);
+    if(sockid<0){
+        printf("Couldn't create socket");
+        return -1;
+    }
+    printf("socket created");
+
+    server.sin_family=AF_INET;
+    server.sin_port=htons(2000);
+    server.sin_addr.s_addr=inet_addr("127.0.0.1");
+    if(bind(sockid,(struct sockaddr*)&server,sizeof(server))<0){
+        printf("Couldn't bind");
+        return -1;
+    }
+    printf("Binding done");
+    if(listen(sockid,1)<0){
+        printf("Couldn't listen");
+        return -1;
+    }
+    printf("Listening...");
+    client_size=sizeof(client);
+    client_sock=accept(sockid,(struct sockaddr*)&client,sizeof(client));
+    if(client_sock<0){
+        printf("Couldn't accept");
+        return -1;
+    }
+    if(recv(client_sock,c_message,sizeof(c_message),0)){
+        printf("Couldn't receive");
+        return -1;
+    }
+    print("\nClient message: %s\n",c_message);
+    printf("Enter the message to be send: ");
+    scanf("%s",&s_message);
+    if(send(client_sock,s_message,strlen(s_message),0)<0){
+        printf("Couldn't Send");
+        return -1;
+    }
+    close(client_sock);
+    close(sockid);
+    return 0;
 }
-printf("Socket created successfully\n");
-server_addr.sin_family = AF_INET;
-server_addr.sin_port = htons(2000);
-server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-if (bind(socket_desc,(struct sockaddr*)&server_addr,sizeof(server_addr))<0) {
-printf("Couldn't blind to the port\n");
-return -1;
-}
-printf("\nDone with binding");
-printf("\nListening for incoming connections...");
-2
-if (listen(socket_desc,1) < 0) {
-printf("Error while listening\n");
-return -1;
-}
-printf("Listening for incoming connections...");
-client_size = sizeof(client_addr);
-client_sock = accept(socket_desc,(struct sockaddr*)&client_addr,&client_size);
-if (client_sock < 0) {
-printf("Can't accept!!!");
-return -1;
-}
-printf("Client connected to ip: %s & port : %i\n",inet_ntoa(client_addr.sin_addr),htons(client_addr.sin_port));
-if (recv(client_sock,client_message,sizeof(client_message),0)
-< 0) {
-printf("Couldn't recieve");
-return -1;
-}
-printf("\nMessage from client: %s\n",client_message);
-printf("Enter server message: ");
-scanf("%s",server_message);
-if (send(client_sock,server_message,strlen(server_message),0) <0) {
-printf("Can't send\n");
-return -1;
-}
-close(client_sock);
-close(socket_desc);
-return 0; }
-}
+
+
